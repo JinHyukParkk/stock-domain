af758212f29a3dbe653a22334c0fbc667481d487
hyuk <pjh08190819@naver.com>
2022-11-01 02:25:40 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/stock/stockdomain/StockDomainApplication.java
@@ -0,0 +1,13 @@
+package com.stock.stockdomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockDomainApplication.class, args);
+    }
+
+}
