56e57b4c59bfb31cacd377ea661a41538239b481
hyuk <hyuk@woowahan.com>
2023-07-27 00:39:39 +0900

    feat : Stock 에서 name 추가

--- src/main/java/com/stock/stockdomain/model/Stock.java
+++ src/main/java/com/stock/stockdomain/model/Stock.java
@@ -5,9 +5,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Getter
 public class Stock {
 
     @Id
     @GeneratedValue
     private Long id;
+
+    private String name;
 }
